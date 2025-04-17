2c09f6e8018f11dae4b8fd5b7c1e4c29a54fafc5
mcanto <mcanto@frba.utn.edu.ar>
2025-04-17 15:40:16 -0300

    feat: se agrega configuraciones de paleta de colores y tipografia

---
+++ constants/colors.js
@@ -0,0 +1,25 @@
+const Colors = {
+  // Paleta principal
+  primary: '#2C507F', 
+  secondary: '#949494',
+  accent: '#EB6E24', 
+
+  // Texto
+  textOnLight: '#000000',
+  textOnDark: '#FFFFFF',
+
+  // Fondos
+  backgroundLight: '#FFFFFF',
+  backgroundDark: '#000000',
+
+  // Texto en botones
+  buttonTextLight: '#FFFFFF',
+  buttonTextDark: '#000000',
+
+  // Descripciones extra
+  primaryRGB: 'rgb(44, 80, 127)',
+  secondaryRGB: 'rgb(148, 148, 148)',
+  accentRGB: 'rgb(235, 110, 36)',
+};
+
+export default Colors;
---
+++ constants/typography.js
@@ -0,0 +1,39 @@
+// constants/typography.js
+
+const Typography = {
+    // Titulares (Quicksand)
+    titleLight: {
+      fontFamily: 'Quicksand-Light',
+      fontWeight: '300',
+      fontSize: 24,
+    },
+    titleMedium: {
+      fontFamily: 'Quicksand-Medium',
+      fontWeight: '500',
+      fontSize: 26,
+    },
+    titleBold: {
+      fontFamily: 'Quicksand-Bold',
+      fontWeight: '700',
+      fontSize: 28,
+    },
+  
+    // Texto general y aclaraciones (Roboto)
+    bodyLight: {
+      fontFamily: 'Roboto-Light',
+      fontWeight: '300',
+      fontSize: 16,
+    },
+    bodySmall: {
+      fontFamily: 'Roboto-Light',
+      fontWeight: '300',
+      fontSize: 14,
+    },
+    buttonBold: {
+      fontFamily: 'Roboto-Bold',
+      fontWeight: '700',
+      fontSize: 16,
+    },
+  };
+  
+  export default Typography;
